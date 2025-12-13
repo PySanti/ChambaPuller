388ebc3869d8fc93635e7625d07dfbca622cf8cf
Santiago <santiagodeandrade4@gmail.com>
2025-12-13 07:30:08 -0400

    Implementacion de archivo .env

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from google import genai
+import os
+
+load_dotenv()
+key = os.getenv("GEMINI_API_KEY")
+
+
+client = genai.Client(api_key=key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
